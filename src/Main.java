8b0786df539c0035c890afe2190e3fd80187d05b
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-09-02 21:44:05 -0400

    JAVA-011 - Agregar menu con while

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,26 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        System.out.print("Ingrese nombre: ");
-        String nombre = teclado.nextLine();
-        System.out.print("Ingrese carrera: ");
-        String carrera = teclado.nextLine();
-        System.out.print("Ingrese edad: ");
-        int edad = teclado.nextInt();
-        Estudiante estudiante = new Estudiante(
-                nombre,
-                carrera,
-                edad
-        );
-        if (edad < 18) {
-            System.out.println("Estudiante menor de edad.");
-        } else if (edad < 25){
-            System.out.println("Estudiante joven.");
-        } else {
-            System.out.println("Estudiante adulto.");
-        }
-        estudiante.mostrarInformacion();
-        for (int i=1;i<=5;i++ ){
-            System.out.println("Contando al estudiante nro "+i);
+        int op = -1;
+        while (op != 0) {
+            Scanner teclado = new Scanner(System.in);
+            System.out.println("=== SISTEMA DUOC ===");
+            System.out.println("1. Mostrar estado");
+            System.out.println("2. Procesar operación");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+            op = teclado.nextInt();
+
+            if (op == 1) {
+                System.out.println("Mostrando Estado.");
+            } else if (op == 2) {
+                System.out.println("Procesando Informacion.");
+            } else if (op== 0){
+                System.out.println("Saliendo del Sistema.");
+            } else {
+                System.out.println("Ingrese una opcion valida");
+            }
+
         }
 
     }
